911f3a28c69a19e1facc85660dddc399acf598eb
Dathuss <34245959+Dathuss@users.noreply.github.com>
2026-01-13 17:12:22 +0100

    main: print tokens

--- cc1/src/main.rs
+++ cc1/src/main.rs
@@ -1,7 +1,7 @@
 use cc1::{lexer, source_manager::SourceManager};
 
 fn main() {
-	const FILE_NAME: &'static str = "b_pp.c";
+	const FILE_NAME: &str = "b_pp.c";
 
 	let mut args = std::env::args();
 	args.next();
@@ -12,7 +12,7 @@ fn main() {
 	let contents = std::fs::read_to_string(file_name).unwrap();
 	let source = SourceManager::from(&*contents, file_name);
 	let lexer = lexer::Lexer::from(&source);
-	for _token in lexer {
-		// println!("{:?}", token);
+	for token in lexer {
+		println!("{:?}", token);
 	}
 }
