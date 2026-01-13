1444c549dbac2be2bafbe11f60be1aa17c9bc06a
Dathuss <34245959+Dathuss@users.noreply.github.com>
2026-01-13 17:12:22 +0100

    simplify c_*_pats

--- cc1/src/lexer/mod.rs
+++ cc1/src/lexer/mod.rs
@@ -13,17 +13,13 @@ pub struct Lexer<'src> {
 
 macro_rules! c_ident_start_pat {
 	() => {
-		b'a' | b'b' | b'c' | b'd' | b'e' | b'f' | b'g' | b'h' | b'i' | b'j' | b'k' | b'l' |
-		b'm' | b'n' | b'o' | b'p' | b'q' | b'r' | b's' | b't' | b'u' | b'v' | b'w' | b'x' |
-		b'y' | b'z' | b'A' | b'B' | b'C' | b'D' | b'E' | b'F' | b'G' | b'H' | b'I' | b'J' |
-		b'K' | b'L' | b'M' | b'N' | b'O' | b'P' | b'Q' | b'R' | b'S' | b'T' | b'U' | b'V' |
-		b'W' | b'X' | b'Y' | b'Z' | b'_'
+		b'a'..=b'z' | b'A'..=b'Z' | b'_'
 	}
 }
 
 macro_rules! c_integer_pat {
 	() => {
-		b'0' | b'1' | b'2' | b'3' | b'4' | b'5' | b'6' | b'7' | b'8' | b'9'
+		b'0'..=b'9'
 	};
 }
 
