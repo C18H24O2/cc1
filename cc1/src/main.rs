be639bb48f7796000dc6161609e7e3ca77c1f23b
Dathuss <34245959+Dathuss@users.noreply.github.com>
2026-01-13 17:12:22 +0100

    cc1 main skeleton

---
+++ cc1/src/main.rs
@@ -0,0 +1,18 @@
+use cc1::{lexer, source_manager::SourceManager};
+
+fn main() {
+	const FILE_NAME: &'static str = "b_pp.c";
+
+	let mut args = std::env::args();
+	args.next();
+	let arg = args.next();
+
+	let file_name = if let Some(x) = &arg { x.as_str() } else { FILE_NAME };
+
+	let contents = std::fs::read_to_string(file_name).unwrap();
+	let source = SourceManager::from(&*contents, file_name);
+	let lexer = lexer::Lexer::from(&source);
+	for _token in lexer {
+		// println!("{:?}", token);
+	}
+}
